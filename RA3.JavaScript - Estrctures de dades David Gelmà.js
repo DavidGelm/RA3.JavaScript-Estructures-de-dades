f524bc95449b588503562680931384af68f102e6
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-22 17:39:07 +0100

    Exercici 4 Javascript Estructures de Dades

--- RA3.JavaScript - Estrctures de dades David Gelmà.js
+++ RA3.JavaScript - Estrctures de dades David Gelmà.js
@@ -1,20 +1,13 @@
-let array1 = [1, 2, 3, 4]
-let array2 = [3, 4, 5, 6]
+let array1 = [1, 2, 3, 4];
+let array2 = [3, 4, 5, 6];
 
-
-let arrayFusionat = []
-
-
+let setFusionat = new Set();
 for (let i in array1) {
-    if (!arrayFusionat.includes(array1[i])) {
-        arrayFusionat.push(array1[i])
-    }
+    setFusionat.add(array1[i]);
 }
-
 for (let element of array2) {
-    if (!arrayFusionat.includes(element)) {
-        arrayFusionat.push(element)
-    }
+    setFusionat.add(element);
 }
+let arrayFusionat = [...setFusionat];
 
-console.log("Array fusionat sense duplicats:", arrayFusionat)
+console.log("Array fusionat sense duplicats:", arrayFusionat);
