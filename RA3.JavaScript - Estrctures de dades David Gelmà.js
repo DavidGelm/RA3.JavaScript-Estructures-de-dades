914f405bd8b442b955f6ede65a509d92aa672bf8
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-25 12:04:51 +0100

    Exercici 7 Javascript Estructures de Dades

--- RA3.JavaScript - Estrctures de dades David Gelmà.js
+++ RA3.JavaScript - Estrctures de dades David Gelmà.js
@@ -1,16 +1,3 @@
-let array_String =["hola","patata","dia","tarda"]
-let array1 = []
-let voc = "a"
-for(let i = 0;i<array_String.length;i++){
-    let string1 = array_String[i]
-    let contador = 0
-    for(let j =0;j<string1.length;j++){
-        if(string1[j]===voc){
-            contador++
-        }
-    }
-    if(contador>1){
-        array1.push(string1)
-    }
-}
-console.log(array1)
+const valor = {'a':1,'b':"patata",'c':true}
+let m = new Map(Object.keys(valor).map(k => [k, valor[k]]))
+console.log(m)
