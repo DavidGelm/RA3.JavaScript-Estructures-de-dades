d0f72e4a4b3d78a950e04e68409169358ede6487
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-25 11:37:50 +0100

    Exercici 6 Javascript Estructures de Dades

--- RA3.JavaScript - Estrctures de dades David Gelmà.js
+++ RA3.JavaScript - Estrctures de dades David Gelmà.js
@@ -1,6 +1,16 @@
-const o = {'a': 1, 'b': 3, 'c': 6}
-
-let m = new Map(Object.keys(o).map(k => [k, o[k]]))
-let cubics = new Map([...m].map(([k, v]) => [k, v ** 3]))
-
-console.log("Map amb els valors cúbics:", cubics)
+let array_String =["hola","patata","dia","tarda"]
+let array1 = []
+let voc = "a"
+for(let i = 0;i<array_String.length;i++){
+    let string1 = array_String[i]
+    let contador = 0
+    for(let j =0;j<string1.length;j++){
+        if(string1[j]===voc){
+            contador++
+        }
+    }
+    if(contador>1){
+        array1.push(string1)
+    }
+}
+console.log(array1)
