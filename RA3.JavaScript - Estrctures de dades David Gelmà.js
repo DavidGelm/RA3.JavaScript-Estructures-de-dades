40f62b3b076b99ad9ffaf8f52bffc42b33e0d702
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-25 13:12:31 +0100

    Exercici 8 Javascript Estructures de Dades

--- RA3.JavaScript - Estrctures de dades David Gelmà.js
+++ RA3.JavaScript - Estrctures de dades David Gelmà.js
@@ -1,3 +1,14 @@
-const valor = {'a':1,'b':"patata",'c':true}
-let m = new Map(Object.keys(valor).map(k => [k, valor[k]]))
-console.log(m)
+function esPalindrom(paraula) {
+     const cua = []
+     for (let lletra of paraula) {
+          cua.push(lletra)
+     }
+     let inversa = ""
+     for (let i = cua.length - 1; i >= 0; i--) {
+          inversa += cua[i]
+     }
+     return paraula === inversa
+}
+
+console.log(esPalindrom("civic"))
+console.log(esPalindrom("hola"))
